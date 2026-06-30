0b8b50d78fce90ffcfd5bcb4d6f67ce73a904d84
A-Catzin <tehcatzinangel6@gmail.com>
2026-06-30 10:17:56 -0500

    fix(deploy): quitar image config + vercel dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,4 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind()],
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/noop'
-    }
-  },
 });
