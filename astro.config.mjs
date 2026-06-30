d97d7d9b9fc8b9a071903be5b3536b70d05a71d1
A-Catzin <tehcatzinangel6@gmail.com>
2026-06-30 10:12:18 -0500

    fix(deploy): Node 24 + Sharp noop para Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  },
 });
